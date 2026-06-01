e4324a7e3c3fd6ff03100bd84585a24418eb5d83
Sumanth Udupi <109497622+SumanthUdupi@users.noreply.github.com>
2026-06-01 19:26:01 +0530

    updates

--- .open-next/cloudflare/next-env.mjs
+++
@@ -1,3 +0,0 @@
-export const production = {"COMMAND_SECRET":"sudhama-dev-secret-32chars-ok99"};
-export const development = {"COMMAND_SECRET":"sudhama-dev-secret-32chars-ok99"};
-export const test = {};
