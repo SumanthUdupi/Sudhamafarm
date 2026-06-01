c0cf149c1dfcc480b8ea243366c0758fa91ba484
Sumanth Udupi <sumanthudupi858@gmail.com>
2026-06-01 07:26:23 +0530

    updates

--- .next/types/validator.ts
+++ .next/types/validator.ts
@@ -101,6 +101,15 @@ type RouteHandlerConfig<Route extends AppRouteHandlerRoutes = AppRouteHandlerRou
   type __Unused = __Check
 }
 
+// Validate ../../src/app/invite/route.ts
+{
+  type __IsExpected<Specific extends RouteHandlerConfig<"/invite">> = Specific
+  const handler = {} as typeof import("../../src/app/invite/route.js")
+  type __Check = __IsExpected<typeof handler>
+  // @ts-ignore
+  type __Unused = __Check
+}
+
 
 
 
---
+++ .open-next/cloudflare/next-env.mjs
@@ -0,0 +1,3 @@
+export const production = {"COMMAND_SECRET":"sudhama-dev-secret-32chars-ok99"};
+export const development = {"COMMAND_SECRET":"sudhama-dev-secret-32chars-ok99"};
+export const test = {};
